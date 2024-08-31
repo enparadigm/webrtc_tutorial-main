4b922f765f6499fb1431f44c610517488fd23a8a
Surya <surya.saravanakumar@sharpsell.ai>
2024-09-01 00:24:00 +0530

    update new text on scren

--- lib/main.dart
+++ lib/main.dart
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("POC"),
+        title: Text("This is test site"),
       ),
       body: Column(
         children: [
